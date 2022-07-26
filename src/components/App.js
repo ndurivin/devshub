4525466d237540adf212c22abe7764c0e65ee971
Vinn Nduri <ndurivo@gmail.com>
2022-07-26 22:57:07 +0300

    Import the Routes & Route and add route paths to app components

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,10 @@
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Devcard from './Devcard';
 import Navbar from './Navbar';
+import Home from './Home';
+import About from './About';
+import Search from './Search';
+import Admin from './Admin';
 
 function App() {
   return (
@@ -10,6 +14,12 @@ function App() {
         <Navbar /> <br/>
         <Devcard />
     </div>
+    <Routes>
+      <Route exact path='/' element={<Home/>}></Route>
+      <Route exact path='/about' element={<About/>}></Route>
+      <Route exact path='/search' element={<Search/>}></Route>
+      <Route exact path='/admin' element={<Admin/>}></Route>
+    </Routes>
     </Router>
     
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -24,7 +24,7 @@ function Navbar() {
               </li>
             </ul>
             <form className="d-flex" role="search">
-              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+              <input className="form-control me-2" type="search" placeholder="Search by technology stack" aria-label="Search"/>
               <button className="btn btn-outline-success" type="submit">Search </button>
             </form>
           </div>
