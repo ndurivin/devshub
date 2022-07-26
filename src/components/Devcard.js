ce35213d05240cbbc5ea54cb4bba703506a4416e
Vinn Nduri <ndurivo@gmail.com>
2022-07-26 21:49:01 +0300

    Add the Devcard component

--- src/components/Devcard.js
+++ src/components/Devcard.js
@@ -1,11 +1,48 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 
 function Devcard() {
   return (
       <div>
-        
-    </div>
+        <div className="row row-cols-1 row-cols-md-4 g-4">
+          <div className="col">
+            <div className="card h-100">
+              <img src="..." className="card-img-top" alt="developer-pic"/>
+              <div className="card-body">
+                <h5 className="card-title">Developer Name</h5>
+                <p className="card-text">This is a longer card.</p>
+              </div>
+            </div>
+          </div>
+          <div className="col">
+            <div className="card h-100">
+              <img src="..." className="card-img-top" alt="developer-pic"/>
+              <div className="card-body">
+                <h5 className="card-title">Developer Name</h5>
+                <p className="card-text">This is a short card.</p>
+              </div>
+            </div>
+          </div>
+          <div className="col">
+            <div className="card h-100">
+              <img src="..." className="card-img-top" alt="developer-pic"/>
+              <div className="card-body">
+                <h5 className="card-title">Developer Name</h5>
+                <p className="card-text">This is a longer card.</p>
+              </div>
+            </div>
+          </div>
+          <div className="col">
+          <div className="card h-100">
+            <img src="..." className="card-img-top" alt="developer-pic"/>
+            <div className="card-body">
+              <h5 className="card-title">Developer Name</h5>
+              <p className="card-text">This is a longer card.</p>
+            </div>
+          </div>
+          </div>
+        </div>
+      </div>
   )
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,8 @@ function Navbar() {
       <nav className="navbar navbar-expand-lg bg-primary">
         <div className="container-fluid">
           <Link to="/" className="navbar-brand">Devshub</Link>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" 
+                        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
