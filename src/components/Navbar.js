7e55512c2395d2f2e4bab0b2b0f5ebf519edaf39
Vinn Nduri <ndurivo@gmail.com>
2022-07-26 20:40:43 +0300

    Delete the app.css file and add bootstrap links for css and  js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 function Navbar() {
   return (
     <div>
-      <nav className="navbar navbar-expand-lg bg-light">
+      <nav className="navbar navbar-expand-lg bg-primary">
         <div className="container-fluid">
           <Link to="/" className="navbar-brand">Devshub</Link>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
