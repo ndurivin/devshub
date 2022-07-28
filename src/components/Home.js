fd394792d8b176ac564e0d7a4a3d5d665a33d17d
Vinn Nduri <ndurivo@gmail.com>
2022-07-28 11:54:14 +0300

    Fix package.json file

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,16 @@
 import React from 'react'
+// import { Link } from 'react-router-dom';
 
 function Home() {
   return (
       
-    <div className='container'>
+      <div className='container'>
         <h1 className="text-center" style={{color:"blue"}}>Welcome to DevsHub</h1> 
         <h3 className="text-center" style={{fontStyle:"italic", borderRadius: "12px"}}>Home of the best software developers</h3> <br/>
         <img src="https://t4.ftcdn.net/jpg/04/19/26/97/360_F_419269782_9LsP3TQndMVnZ2j3ZhTPhMjaqQpFAth9.jpg" className="card-img-top img-fluid" alt=""/>
-
-        
+          {/* <form className="d-flex" role="search">
+              <Link to="/search" className="nav-link justify-content-center">Search for Developers</Link>
+         </form> */}
 
     </div>
   )
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,33 +1,29 @@
 import React, { useState, useEffect }from 'react'
 import DevCard from './DevCard';
 
-const devsUrl = "https://quiet-hamlet-90428.herokuapp.com/devs";
+const api_Url = "https://quiet-hamlet-90428.herokuapp.com/devs";
 
 function Search() {
 
-  // const [search, setSearch] = useState("")
+  const seachDevs = async (name) => {
+    const res = await fetch(`${api_Url}&s=${name}`)
+    const data = await res.json();
+  }
+useEffect(() => {
+  seachDevs()
+
+},[]);
+  
 
   
   return (
     <div className='container'>
-      <form className="d-flex" role="search">
-         <input className="form-control" type="search" placeholder="Search by technology stack..." aria-label="Search"
-          // onChange={(e) => setSearch(e.target.value)}
-         />
-         <button className="btn btn-outline-alert" type="submit">Search </button>
-      </form>
-      <div className='py-4 container'>
-      <div className="row cols-3 justify-content-center">
-        <div className="col-11 col md-6 col-lg-3 mx-0 mb-4">
-        {/* <div className="card h-100 shadow"> */}
-            {/* {detailCards} */}
-        {/* </div> */}
-        </div>
-      </div>
-    </div>
+      
 
     </div>
   )
 }
 
 export default Search;
+
+
